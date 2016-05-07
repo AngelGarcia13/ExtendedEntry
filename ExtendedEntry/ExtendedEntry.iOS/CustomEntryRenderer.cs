e5b1c468a52fce1f1a3af86a7689fc7d87121e51
Angel Garcia <angel.rene.garcia13@gmail.com>
2016-05-07 01:25:41 -0400

    Added missing usings.

--- ExtendedEntry/ExtendedEntry.iOS/CustomEntryRenderer.cs
+++ ExtendedEntry/ExtendedEntry.iOS/CustomEntryRenderer.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Text.RegularExpressions;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
